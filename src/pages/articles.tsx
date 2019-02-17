fb510af80805b7fcdbcafd6861d8d3d2ddfc14bf
Francesco Lanciana <francescolanciana@gmail.com>
2019-02-17 20:26:04 +1100

    Added articles page
    Displays articles from prismicJs in a SSR fashion.

---
+++ src/pages/articles.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Link from "next/link";
+import { NextFC } from "next";
+import DefaultLayout from "@Layouts/DefaultLayout/DefaultLayout";
+import { getArticles } from "@Modules/blogApi";
+import { linkResolver } from "@Modules/blogHelpers";
+import { Document } from "@Types/prismic-javascript";
+
+import './articles.scss';
+
+interface ArticlesPageProps {
+    posts: Document[];
+}
+
+const ArticlesPage: NextFC<ArticlesPageProps> = ({ posts }) => (
+    <DefaultLayout>
+        <div className="articles-page">
+            
+            <h1 className="page-title">Articles</h1>
+
+            <main className="articles">
+                <ul className="articles-list">
+                    {posts.map((post, index) => (
+                        <li key={index}>
+                            <Link as={linkResolver(post)} href={`/article?slug=${post.uid}`} passHref>
+                                <a className="article-title">
+                                    <h2>{post.data.title[0].text}</h2>
+                                </a>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </main>
+        </div>
+    </DefaultLayout>
+);
+
+ArticlesPage.getInitialProps = async () => {
+    // Here we call the API and request 5 documents
+    const [, res] = await getArticles({ pageSize: 5 });
+
+    return {
+        posts: res.results,
+    };
+};
+
+export default ArticlesPage;
