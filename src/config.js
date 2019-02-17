a6723a88ecc0a1b9908335cd29ebd5e6c33a614f
Francesco Lanciana <francescolanciana@gmail.com>
2019-02-17 20:17:56 +1100

    Created a new base for the page components
    This automatically enhances the SEO of every single page, while also providing a single point to include things that affect the entire app (e.g. our app wide styles)

---
+++ src/config.js
@@ -0,0 +1,22 @@
+export const PRISMIC_API_URL = "https://mosaic.cdn.prismic.io/api/v2";
+
+export const DEFAULT_SEO = {
+    title: "F&J",
+    description: "A blog detailing every detail on our journey to make a startup",
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: "https://www.fandj.blog",
+        title: "F&J",
+        description: "A blog detailing detailing our journey to create a startup",
+        image:
+            "https://images.unsplash.com/photo-1508873787497-1b513a18217a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=4896&q=80",
+        site_name: "F&J",
+        imageWidth: "1200",
+        imageHeight: "1200",
+    },
+    twitter: {
+        handle: "@fandjblog",
+        cardType: "summary_large_image",
+    },
+};
