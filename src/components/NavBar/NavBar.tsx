727346d7a819825e2e174ecf2d16b8b62610de3e
Francesco Lanciana <francescolanciana@gmail.com>
2019-03-01 00:46:55 +1100

    Updated styling for the navbar
    We now have an underline on the active tab, as well as more space between links and a larger clicking area

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,24 @@
 /**
- * A navigation component that dissapears while the user is read an article (i.e. is 
+ * A navigation component that dissapears while the user is read an article (i.e. is
  * scrolling down the page)
  * Related issues: https://github.com/zeit/next.js/issues/796
  */
 
 import React from "react";
-import Link from 'next/link'
+import Link from "next/link";
+import { withRouter, SingletonRouter } from "next/router";
 
 import { useScrollBasedPin } from "@Hooks/window-hooks.ts";
 
 import "./NavBar.scss";
 
-const NavBar: React.FC<{}> = () => {
+interface NavBarProps {
+    router: SingletonRouter;
+}
+
+const NavBar: React.FC<NavBarProps> = ({ router }) => {
     const isPinned = useScrollBasedPin(true, { pinThreshold: 50 });
+    const { pathname } = router;
 
     return (
         <header className="navbar f-f-montserrat" data-show={isPinned}>
@@ -20,9 +26,21 @@ const NavBar: React.FC<{}> = () => {
                 <div className="logo-container">F&amp;J</div>
                 <nav className="site-navigation-container">
                     <ul className="site-navigation-links">
-                        <li className="site-navigation-link"><Link prefetch href="/articles"><a>Articles</a></Link></li>
-                        <li className="site-navigation-link"><Link prefetch href="/experiments"><a>Experiments</a></Link></li>
-                        <li className="site-navigation-link"><Link prefetch href="/about"><a>About</a></Link></li>
+                        <li className="site-navigation-link">
+                            <Link prefetch href="/articles">
+                                <a data-active={pathname.includes("/articles")}>Articles</a>
+                            </Link>
+                        </li>
+                        <li className="site-navigation-link">
+                            <Link prefetch href="/experiments">
+                                <a data-active={pathname.includes("/experiments")}>Experiments</a>
+                            </Link>
+                        </li>
+                        <li className="site-navigation-link">
+                            <Link prefetch href="/about">
+                                <a data-active={pathname.includes("/about")}>About</a>
+                            </Link>
+                        </li>
                     </ul>
                 </nav>
             </div>
@@ -30,4 +48,4 @@ const NavBar: React.FC<{}> = () => {
     );
 };
 
-export default NavBar;
+export default withRouter(NavBar);
